75ae60110ff554d65fdd23008b46dcbe81f4a5a5
liuxh616 <liuxh616@126.com>
2020-08-11 21:42:15 +0800

    提交DI项目的测试代码

---
+++ day01-demo04-di/src/main/java/com/fengfang/domain/Address.java
@@ -0,0 +1,19 @@
+package com.fengfang.domain;
+
+/**
+ * TODO
+ *
+ * @author liuxh
+ * @date 2020/8/11 21:38
+ **/
+public class Address {
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ day01-demo04-di/src/main/java/com/fengfang/domain/Student.java
@@ -0,0 +1,24 @@
+package com.fengfang.domain;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * TODO
+ *
+ * @author liuxh
+ * @date 2020/8/11 21:38
+ **/
+public class Student {
+    private String name;
+    private Address address;
+    private String[] books;
+    private List<String> hobys;
+    private Integer age;
+    private Map<String,String> card;
+    private Set<String> games;
+    private Properties info;
+    private String wife;
+}
